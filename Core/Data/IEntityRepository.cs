aab1d296d21cf51574e450ce0972e3d66d413fcd
zeljkosamed <smdkthyli@gmail.com>
2021-12-26 00:20:34 +0300

    IEntityRepository ve IService oluşturuldu.

---
+++ Core/Data/IEntityRepository.cs
@@ -0,0 +1,23 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Data
+{
+    public interface IEntityRepository<T> where T : class, IEntity, new()
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<IEnumerable<T>> Find(Expression<Func<T, bool>> predicate);
+        Task<T> SingleOrDefaultAsync(Expression<Func<T, bool>> predicate);
+        Task AddAsync(T entity); 
+        Task AddRangeAsync(IEnumerable<T> entities); 
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+        T Update(T entity);
+    }
+}
--- Entities/Concrete/Category.cs
+++ Entities/Concrete/Category.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core.Entities;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Entities.Concrete
 {
-    public class Category
+    public class Category : IEntity
     {
         public Category()
         {
